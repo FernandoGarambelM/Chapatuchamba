3c1af95b7083c85f042ea9f85b069c570ad25fa2
Fernando Garambel <fgarambel@unsa.edu.pe>
2026-01-07 23:42:51 -0500

    feat: add certifications relationship to Student model
    - Added @OneToMany relationship to StudentCertification
    - Enables certification bonus calculation in scoring system

--- backend/src/main/java/com/chapatuchamba/hub/user/model/Student.java
+++ backend/src/main/java/com/chapatuchamba/hub/user/model/Student.java
@@ -3,6 +3,7 @@ package com.chapatuchamba.hub.user.model;
 import jakarta.persistence.*;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
+import java.util.List;
 
 @Data
 @EqualsAndHashCode(callSuper = true)
@@ -16,12 +17,15 @@ public class Student extends User {
 
     private String name;
     private String university;
-    private String major; // Carrera
+    private String major;
 
     @Column(columnDefinition = "TEXT")
-    private String bio; // Resumen
+    private String bio;
 
     @Column(name = "global_score")
     @Builder.Default
     private Integer globalScore = 0;
+
+    @OneToMany(mappedBy = "student", fetch = FetchType.LAZY)
+    private List<StudentCertification> certifications;
 }
